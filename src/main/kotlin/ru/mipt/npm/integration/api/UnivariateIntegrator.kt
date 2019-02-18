6cafb8df10d89318d95476cadd8e68829d46a8fd
ggoyman <35761231+meelly@users.noreply.github.com>
2019-02-19 00:45:39 +0300

    Update UnivariateIntegrator.kt
    Trapezoidal integration method implementation

--- src/main/kotlin/ru/mipt/npm/integration/api/UnivariateIntegrator.kt
+++ src/main/kotlin/ru/mipt/npm/integration/api/UnivariateIntegrator.kt
@@ -2,13 +2,24 @@ package ru.mipt.npm.integration.api
 
 interface UnivariateIntegrator {
     fun integrate(
-        from: Double,
-        to: Double,
-        step: Double = (to - from) / DEFAULT_STEPS,
-        function: (Double) -> Double
+            from: Double,
+            to: Double,
+            stepsNum: Int = DEFAULT_STEPS_NUM,
+            function: (Double) -> Double
     ): Double
 
     companion object {
-        const val DEFAULT_STEPS = 100
+        const val DEFAULT_STEPS_NUM = 40
+    }
+}
+
+object TrapezoidalIntegrator: UnivariateIntegrator {
+    override fun integrate(from: Double, to: Double, stepsNum: Int, function: (Double) -> Double): Double {
+        var sum: Double = 0.0
+        val step: Double = (to-from)/stepsNum
+        for (i in 1..stepsNum) {
+            sum += function(from + (i - 1) * step) + function(from + i * step)
+        }
+        return step * 0.5 * sum
     }
 }
