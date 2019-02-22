543ee657ad8bdb71490e64b56a34b3c7b53f705e
ggoyman <35761231+meelly@users.noreply.github.com>
2019-02-22 21:55:40 +0300

    New interface. Iterative trapz integration.

--- src/main/kotlin/ru/mipt/npm/integration/api/UnivariateIntegrator.kt
+++ src/main/kotlin/ru/mipt/npm/integration/api/UnivariateIntegrator.kt
@@ -1,25 +1,29 @@
 package ru.mipt.npm.integration.api
 
 interface UnivariateIntegrator {
+    /**
+     * Returns the integral of the given function over the interval (from, to).
+     * from - starting point of the integration domain
+     * to   - end point of the integration domain
+     * relTol, absTol - desired relative and absolute tolerance
+     * iniStepsNum - initial number of grid intervals
+     * maxIterNum] - Maximum number of the doubling grid resolution
+     * function - function to be integrated
+     */
     fun integrate(
             from: Double,
             to: Double,
-            stepsNum: Int = DEFAULT_STEPS_NUM,
+            relTol : Double = DEFAULT_REL_TOL,
+            absTol : Double = DEFAULT_ABS_TOL,
+            iniStepsNum: Int = DEFAULT_INITIAL_STEPS_NUM,
+            maxIterNum : Int = DEFAULT_MAX_ITER_NUM,
             function: (Double) -> Double
     ): Double
 
     companion object {
-        const val DEFAULT_STEPS_NUM = 40
-    }
-}
-
-object TrapezoidalIntegrator: UnivariateIntegrator {
-    override fun integrate(from: Double, to: Double, stepsNum: Int, function: (Double) -> Double): Double {
-        var sum: Double = 0.0
-        val step: Double = (to-from)/stepsNum
-        for (i in 1..stepsNum) {
-            sum += function(from + (i - 1) * step) + function(from + i * step)
-        }
-        return step * 0.5 * sum
+        const val DEFAULT_REL_TOL: Double = 1e-8
+        const val DEFAULT_ABS_TOL: Double = 1e-8
+        const val DEFAULT_INITIAL_STEPS_NUM = 16
+        const val DEFAULT_MAX_ITER_NUM: Int = 16
     }
 }
